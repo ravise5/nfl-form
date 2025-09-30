ba76d4b84ca0bc544a18a8902c9d4dffeda6dab7
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2025-09-30 18:31:40 +0530

    add team selection component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['range'];
+let customComponents = ['range', 'team-selection'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
