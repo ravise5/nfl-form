65596ffaf6c1b9ea0b569e9e63bc69cff27cc903
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2025-10-01 15:08:01 +0530

    fix team selection component

--- blocks/form/components/team-selection/team-selection.js
+++ blocks/form/components/team-selection/team-selection.js
@@ -8,7 +8,7 @@ import { createOptimizedPicture } from '../../../../scripts/aem.js';
  * @param {string} formId - The unique identifier of the form.
  */
 export default async function decorate(element, fieldJson, parentElement, formId) {
-  const { enumNames: imagePaths, enum: altText } = fieldJson;
+  const { enum: imagePaths, enumNames: altText } = fieldJson;
   const { selectionType } = fieldJson.properties;
 
   element.classList.add('team-selection');
