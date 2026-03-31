c10711698fcb0b34d38e28ceeba7c6f8b8b6b77e
lovelymandal16 <lovelymandal16@gmail.com>
2026-03-31 23:53:39 +0530

    removing drag and drop text

--- blocks/form/components/file/file.js
+++ blocks/form/components/file/file.js
@@ -151,12 +151,13 @@ function fileElement(file, index) {
 function createDragAndDropArea(wrapper, field) {
   const input = wrapper.querySelector('input');
   const customButtonText = field?.properties?.['fd:buttonText'] || fileAttachmentText;
-  const customDragDropText = field?.properties?.dragDropText || '';
+  const customDragDropText = field?.properties?.dragDropText || dragDropText;
   const dragArea = `
     <div class="file-dragIcon"></div>
-    <div class="file-dragText">${customDragDropText}</div>
+   
     <button class="file-attachButton" type="button">${customButtonText}</button>
   `;
+  // <div class="file-dragText">${customDragDropText}</div>
   const dragContainer = document.createElement('div');
   if (input.title) {
     dragContainer.title = stripTags(input.title, '');
